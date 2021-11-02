f6dc597677477587032385d2aa401250902a3cf3
milan199608 <executive10000@gmail.com>
2021-11-03 02:40:26 +0530

    about page api call done

--- src/containers/pages/About/About.js
+++ src/containers/pages/About/About.js
@@ -1,36 +1,39 @@
 import React,{useState,useEffect} from 'react';
-
 import "./About.css";
 import {PageContainer} from '../../';
 
 const About = () => {
-  const [items, setItems] = useState("")
+  const [items, setItems] = useState([]);
   useEffect(()=>{
-    fetch("https://www.mydailyva.com/dev/api/getAllPages").then(res=>res.json()).then(
+    var data = JSON.stringify({
+      "page_id": 2
+    });
+
+    fetch("https://www.mydailyva.com/dev/api/getASinglePage",{
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body:data,
+  })
+    .then(res=>res.json()).then(
       (result)=>{
-        setItems(result);
+        setItems(result.data);
       }
     )
   },[])
   const metaTitle='About Us - Mydailyva Virtual Assistants';
   const metaDescription='About Us - Mydailyva Virtual Assistants';
-
+  console.log(items.page_description);
     return (
       <PageContainer title={metaTitle} description={metaDescription}>
-        {console.log(items)}
-        
+       
             <section className="intro-box terms-box-center">
+           
         <div className="row align-items-center justify-content-center">
       
           <div className="col-lg-10 col-12 terms-box-inside">
-          {
-        Object.keys(items).map((item,id)=>(
-            <p key={id}>
-          
-              {items[item].page_description}
-
-            </p>
-            ))}
+         <h2>{items.page_title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: items.page_description }} />
+         
 
           </div>
      
